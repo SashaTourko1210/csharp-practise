edc85c599115ecc6e333767650dd0bc3c0089798
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-16 13:11:30 +0300

    Task1 done

---
+++ Tsk1/Task1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short i = 2;
+            object o = i;
+            sbyte q = (sbyte)(short)o;
+            Console.WriteLine("{0} = {1}", q,q.GetType());
+        }
+    }
+}
