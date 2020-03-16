915c7c7a64c3bfccba5c4a9210dd71bc3c2cf35b
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-16 13:28:27 +0300

    Task 2

---
+++ Tsk2/FirstPractise/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arraylist = new ArrayList();
+
+            //int
+            arraylist.Add(45);//true,boxing
+            
+            //bool
+            arraylist.Add(true);//true,boxing
+
+            //string
+            arraylist.Add("Hello!");//false
+
+            //float
+            arraylist.Add(23.45);//true, boxing
+        }
+    }
+}
