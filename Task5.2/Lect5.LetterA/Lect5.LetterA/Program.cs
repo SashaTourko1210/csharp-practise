b6ffd1e1f59bcf41cc6d3d32b0e4fb99274b7a69
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-17 21:41:47 +0300

    Task2

---
+++ Task5.2/Lect5.LetterA/Lect5.LetterA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lect5.LetterA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int countA = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                    countA++;
+            }
+            Console.WriteLine(countA);
+        }
+    }
+}
