4d71c2785ee354e571ed63ade0486162426bf5d0
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-17 21:35:45 +0300

    Task1

---
+++ Task5.1/Lect5.Greeting/Lect5.Greeting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lect5.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = DateTime.Now.Hour;
+
+            if (h>=9 && h<=12)
+            {
+                Console.WriteLine("Good morning, guys!");
+            }
+            else if (h>12 && h<=15)
+            {
+                Console.WriteLine("Good day,guys!");
+            }
+            else if (h>15 && h<=22)
+            {
+                Console.WriteLine("Good night, guys!");
+            }
+
+
+        }
+    }
+}
