7fad71927e8e9ce788e7d838c2fa083b572555d1
sasha <sasha@DESKTOP-ODT1BBB>
2020-03-24 20:12:37 +0300

    Tasks

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = new object[3] {32, 'A', "Hello"};
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            arr[0] = arr[0] + 10;
+            arr[2] = arr[2] + ", guys!";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Task1/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[13];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(51);
+            }
+
+            int max = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+        }
+    }
+}
